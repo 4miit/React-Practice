0494c4f04030b4fc8354c21bb146b693e47e9234
Amit Kumar <152176527+4miit@users.noreply.github.com>
2024-09-07 23:16:00 +0530

    learning-Redux

--- src/Redux/Counter/CounterSlice.ts
+++ src/Redux/Counter/CounterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 // Define the state type
 interface CounterState {
@@ -7,33 +7,34 @@ interface CounterState {
 
 // Initial state
 const initialState: CounterState = {
-  value: 0
-}
+  value: 0,
+};
 
 // Create the slice with reducers, adding type annotations
 export const counterSlice = createSlice({
-  name: 'counterrrr',
+  name: "counterrrr",
   initialState,
   reducers: {
     increment: (state) => {
-      state.value += 1
+      state.value += 1;
     },
     decrement: (state) => {
-      state.value -= 1
+      state.value -= 1;
     },
     multiply: (state) => {
-      state.value *= 2
+      state.value *= 2;
     },
     divide: (state) => {
-      state.value /= 2
+      state.value /= 2;
     },
     incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    }
-  }
-})
+      state.value += action.payload;
+    },
+  },
+});
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement,multiply,divide, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, multiply, divide, incrementByAmount } =
+  counterSlice.actions;
 
-export default counterSlice.reducer
+export default counterSlice.reducer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css'
-import store from './Redux/store'
-import { Provider } from 'react-redux'
-import { Counter } from './Redux/Redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import store from "./Redux/store";
+import { Provider } from "react-redux";
+import { Counter } from "./Redux/Redux";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    
-     <App/>
-     <Provider store={store}>
-         <Counter/>
-     </Provider>
-     
-    
-  </React.StrictMode>,
-)
+    <App />
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  </React.StrictMode>
+);
